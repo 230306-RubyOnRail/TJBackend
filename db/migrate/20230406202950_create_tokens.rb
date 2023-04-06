71d1df479dd6a0d79ea815248834374b8b340428
Joseph Scott <joseph746567@hotmail.com>
2023-04-06 14:54:43 -0600

    I remove column that was not needed and added foreign key

--- db/migrate/20230406202950_create_tokens.rb
+++ db/migrate/20230406202950_create_tokens.rb
@@ -1,12 +1,9 @@
 class CreateTokens < ActiveRecord::Migration[7.0]
   def change
     create_table :tokens do |t|
-      t.integer :token_id
       t.string :token
-      t.integer :user_id
+      t.references :user, null: false, foreign_key: true
 
-      t.timestamps
-    end
-    add_index :tokens, :token_id, unique: true
+          end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_185409) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_203249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "reimbursements", force: :cascade do |t|
-    t.integer "reimbursement_id"
     t.string "title"
     t.string "description"
     t.boolean "approved"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["reimbursement_id"], name: "index_reimbursements_on_reimbursement_id", unique: true
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_reimbursements_on_user_id"
   end
 
   create_table "tokens", force: :cascade do |t|
-    t.integer "token_id"
     t.string "token"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["token_id"], name: "index_tokens_on_token_id", unique: true
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tokens_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "user_id"
     t.string "username"
     t.string "password"
     t.boolean "manager"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_users_on_user_id", unique: true
+    t.index ["id"], name: "index_users_on_id", unique: true
   end
 
+  add_foreign_key "reimbursements", "users"
+  add_foreign_key "tokens", "users"
 end
