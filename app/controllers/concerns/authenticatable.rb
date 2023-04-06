6af868717637062b1b06ecae0e4ff460bff5ab82
doomphd <trumantang99@gmail.com>
2023-04-06 09:06:43 -0700

    updated ruby version 3.2.2

---
+++ app/controllers/concerns/authenticatable.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+require_relative '../../../lib/json_web_token'
+
+module Authenticatable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :authenticate_request!
+    attr_reader :current_user
+  end
+
+  private
+
+  def authenticate_request!
+    token_data = JsonWebToken.decode(token)
+    puts token_data.inspect
+    @current_user = User.find(JsonWebToken.decode(token)['user_id'])
+    render json: { error: 'Not Authorized' }, status: 401 unless current_user
+    render json: { error: 'You are not allowed to perform this action' }, status: 403 unless params[:user_id] && @current_user.id == params[:user_id].to_i
+  end
+
+  def token
+    request.headers['Authorization'].split(' ').last if request.headers['Authorization'].present?
+  end
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 require_relative '../../lib/json_web_token'
 
 class SessionsController < ApplicationController
+  include PrintMessage
   def create
     credentials = JSON.parse(request.body.read)
-    user = {user_id: 'j', email: 'j.doe@gmail.com', password: 'password'}
-    render json: {token: JsonWebToken.encode(user_id: user[:user_id])}, status: :created
+    user = User.where(user: credentials['user']).first
+    if user&.authenticate(credentials['password'])
+      render json: { token: JsonWebToken.encode(user_id: user.id), user_id: user.id}, status: :created
+    else
+      head :unauthorized
+    end
   end
 end
