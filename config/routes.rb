779365214ee316b519099e88af893f6a7c6c5eb2
doomphd <trumantang99@gmail.com>
2023-04-10 09:25:13 -0700

    patch

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   get 'reimbursements', to: 'reimbursements#show'
   #get 'reimbursements/all/:id', to: 'reimbursements#show_all'
   put 'reimbursements/:id', to: 'reimbursements#update'
-  #patch 'reimbursements/:id', to: 'reimbursements#update'
-  delete 'reimbursements/:id', to: 'reimbursements#delete'
+  patch 'reimbursements/:id', to: 'reimbursements#update'
+  delete 'reimbursements', to: 'reimbursements#delete'
   #resources :reimbusements, path: '/reimbursements', only: %i[ index show update destroy ]
   # Defines the root path route ("/")
   # root "articles#index"
