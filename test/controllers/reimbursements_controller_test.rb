a7681b6c06873b0615da772db14ad11a45f7b445
Joseph Scott <joseph746567@hotmail.com>
2023-04-14 06:01:33 -0600

    make some test code for the API

--- test/controllers/reimbursements_controller_test.rb
+++ test/controllers/reimbursements_controller_test.rb
@@ -1,31 +1,28 @@
 require "test_helper"
 
 class ReimbursementsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get reimbursements_index_url
+    assert_response :success
+  end
 
- 
+  test "should get create" do
+    get reimbursements_create_url
+    assert_response :success
+  end
 
-  # test "should get index" do
-  #   get reimbursements_index_url
-  #   assert_response :success
-  # end
+  test "should get show" do
+    get reimbursements_show_url
+    assert_response :success
+  end
 
-  # test "should get create" do
-  #   get reimbursements_create_url
-  #   assert_response :success
-  # end
+  test "should get update" do
+    get reimbursements_update_url
+    assert_response :success
+  end
 
-  # test "should get show" do
-  #   get reimbursements_show_url
-  #   assert_response :success
-  # end
-
-  # test "should get update" do
-  #   get reimbursements_update_url
-  #   assert_response :success
-  # end
-
-  # test "should get delete" do
-  #   get reimbursements_delete_url
-  #   assert_response :success
-  # end
+  test "should get delete" do
+    get reimbursements_delete_url
+    assert_response :success
+  end
 end
