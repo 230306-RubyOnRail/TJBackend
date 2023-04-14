a88691af5571c6ad25f9e4579dee9cb8141276e4
doomphd <trumantang99@gmail.com>
2023-04-14 03:37:23 -0700

    stuffchanged

--- app/controllers/reimbursements_controller.rb
+++ app/controllers/reimbursements_controller.rb
@@ -20,10 +20,10 @@ class ReimbursementsController < ApplicationController
 
   def show
     if @current_user['manager'] == true
-      @user = Reimbursement.select('id', 'title','description', 'approved')
+      @user = Reimbursement.select('user_id', 'id', 'title','description', 'approved')
       render json: @user.as_json, status: :ok
     else
-      @user = Reimbursement.select('id', 'title','description', 'approved').where(user_id: @current_user['id'])
+      @user = Reimbursement.select('user_id', 'id', 'title','description', 'approved').where(user_id: @current_user['id'])
       if @user.empty?
        render json: {message: 'none to display'}, status: :ok
       else
@@ -50,20 +50,29 @@ class ReimbursementsController < ApplicationController
   end
 
   def delete
-    data = JSON.parse(request.body.read)
-    if @current_user['manager'] == true
-      Reimbursement.find(data['id']).delete
-      render json: {message: 'Delete Successful'}, Status: :ok
+    reimbursement = Reimbursement.find(params[:id])
+    if reimbursement.user_id == @current_user.id || @current_user['manager'] == true
+      reimbursement.destroy
+      render json: {message: 'Deleted'}, Status: :ok
     else
-      r = Reimbursement.select('id').where(user_id: @current_user['id'], id: data['id'])
-      if r.empty?
-        puts" I'm Empty!!!!!!!!!!!!!!!!!!!!!!!!!!"
-        render json: {message: 'reimbursement not found!'}, status: :bad_request
-      else
-        Reimbursement.destroy(data['id'])
-        render json: {message: 'Delete Successful'}, status: :ok
-      end
+      render json: { message: 'not right user' }, status: :unauthorized
     end
+
+
+    # data = JSON.parse(request.body.read)
+    # if @current_user['manager'] == true
+    #   Reimbursement.find(data['id']).delete
+    #   render json: {message: 'Delete Successful'}, Status: :ok
+    # else
+    #   r = Reimbursement.select('id').where(user_id: @current_user['id'], id: data['id'])
+    #   if r.empty?
+    #     puts" I'm Empty!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    #     render json: {message: 'reimbursement not found!'}, status: :bad_request
+    #   else
+    #     Reimbursement.destroy(data['id'])
+    #     render json: {message: 'Delete Successful'}, status: :ok
+    #   end
+    # end
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_040702) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_203249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_040702) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.boolean "manager", default: false
+    t.boolean "manager"
     t.index ["id"], name: "index_users_on_id", unique: true
   end
 
